bcaef9ae3f8bbbf758aa5c1549f0918d4032416a
Rawan199812 <rawan.13579@icloud.com>
2021-06-29 09:20:40 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {Head}  from './pages/Head';
 import {Header}  from './pages/Header';
 import {Main}  from './pages/Main';
 import {Footer}  from './pages/Footer';
+import { useState } from 'react'
 
 
 import React from 'react';
 
 function App() {
+  const [title,setTitle] = useState('Cookie Stand Admin');
+
+
   return (
     <div className="App">
-      <Head/>
+      <Head  title={title}/>
       <Header/>
       <Main/>
       <Footer/>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,14 +2,40 @@ import React from 'react'
 
 export const Main = () => {
     return (
-        <div>
+      
             <form class="bg-green-200">
+                <fieldset>
                 <label >
                     Create Cookie Stand
                 </label>
-
+                    <div>
+                        <div>
+                            <div >
+                                <label >location</label>
+                                <input type="text" />
+                            </div>
+                        </div>
+                        <div >
+                            <div >
+                                <div>
+                                    <label >Minimum Customers per Hour</label>
+                                    <input type="number" />
+                                </div>
+                                <div >
+                                    <label >Maximum Customers per Hour</label>
+                                    <input type="number"  />
+                                </div>
+                                <div >
+                                    <label >Average Cookies per Sale</label>
+                                    <input type="number"  />
+                                </div>
+                                <button className="bg-green-600 w-1/4">Create</button>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
             </form>
             
-        </div>
+      
     )
 }
